209e34a971d68bcdec593cea6989c5fb04a191b6
pkg-dot-zip <simon@pkg-dot-zip.com>
2024-01-08 13:40:30 +0100

    Created interface for Scene called IScene

---
+++ RayWork/Objects/IScene.cs
@@ -0,0 +1,26 @@
+﻿namespace RayWork.Objects
+{
+    public interface IScene
+    {
+        public string Label { get; }
+
+        public void Update();
+        public void Render();
+
+        public void Initialize();
+        public void ReInitialize();
+        public void UpdateLoop();
+        public void RenderLoop();
+
+        public void DebugLoop();
+
+        public void Dispose();
+
+        public void AddChild(GameObject gameObject);
+
+        public void RemoveChild(GameObject gameObject);
+
+        public GameObject[] GetChildren();
+        public bool HasChildren();
+    }
+}
